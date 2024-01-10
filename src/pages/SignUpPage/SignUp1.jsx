f35e7d39d989090549efd437a0bc28aa25ab0fdc
yewon_lee <dapang14@naver.com>
2024-01-10 19:39:15 +0900

    SIGNUP : UI 수정

--- src/pages/SignUpPage/SignUp1.jsx
+++ src/pages/SignUpPage/SignUp1.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./SignUp1.css";
 
-function SignUp1() {
+export default function SignUp1() {
+  
   return (
-    <div>SignUp1</div>
-  )
+    <div className="SignUp1page">
+      <div className="SignUp1titleWrap">
+        DASOM
+      </div>
+      <div className="SignUp1check">이미 회원이신가요?</div>
+      <button className="SignUp1loginbutton">로그인 하기</button>
+      <div className="SignUp1contentWrap">
+        <div className="SignUp1inputWrap">
+          <input
+            type="text"
+            className="SignUp1input"
+            placeholder="인증 번호를 입력하세요"/>
+          <div>
+            <button className="SignUp1bottomButton">다솜 부원 인증하기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default SignUp1
