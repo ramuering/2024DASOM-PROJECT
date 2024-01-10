368b0fd6d0360be8e843aa91190abb4de418bc97
jungmin030 <ontonu@naver.com>
2024-01-10 21:12:35 +0900

    신입부원UI 테스트

--- src/pages/ApplyPage/Apply2.jsx
+++ src/pages/ApplyPage/Apply2.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from '../../components/LimitHeader'
+import "./Apply2.css"
 
 function Apply2() {
   return (
-    <div>Apply2</div>
+    <div id="Apply2_main">
+      <Header></Header>
+      <div id="first_forms" class="form">
+
+        <div>
+        <p id="form_title">DASOM 32nd</p>
+        <br></br>
+        <p id="sub_title">다솜 32기에 지원해 주셔서 감사합니다.,<br></br>
+작성해 주신 지원서는 검토 후<br></br>
+서류 결과 발표일에 문자로 연락드리겠습니다.<br></br><br></br>
+
+감사합니다.</p>
+        </div>
+        
+
+      </div>
+    </div>
   )
 }
 
