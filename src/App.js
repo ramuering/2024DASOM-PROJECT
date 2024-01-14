7ffe5b0ea31988f63cc5f207f782d85e87f835c9
namhosung6023 <hosung60023@naver.com>
2024-01-14 14:47:42 +0900

    프록시 관련 수정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import "./App.css";
 import { Outlet, Route, Routes } from "react-router-dom";
 import Main from "./pages/MainPage/Main";
@@ -47,5 +48,28 @@ function App() {
     </div>
   );
 }
+
+
+/*
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
+*/
+
 
 export default App;
