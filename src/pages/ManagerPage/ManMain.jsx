94989ff20bd801e3942afac76dd198da65238933
sooh329 <glay@kakao.com>
2024-01-15 21:14:43 +0900

    ManMain : button css 수정

--- src/pages/ManagerPage/ManMain.jsx
+++ src/pages/ManagerPage/ManMain.jsx
@@ -7,10 +7,10 @@ function ManMain() {
     <div className='manMain'>
       <Header />
       <p className='manMain-title'>관리자</p>
-      <button className='manMain-button'><a className='manMain-a' href="./ManMember">회원 관리</a></button>
-      <button className='manMain-button'><a className='manMain-a' href="./ManStudy">스터디 관리</a></button>
-      <button className='manMain-button'><a className='manMain-a' href="./ManProject">프로젝트 관리</a></button>
-      <button className='manMain-button'><a className='manMain-a' href="./ManApply">모집 관리</a></button>
+      <a className='manMain-a' href="./manMember"><button className='manMain-button'>회원 관리</button></a>
+      <a className='manMain-a' href="./manStudy"><button className='manMain-button'>스터디 관리</button></a>
+      <a className='manMain-a' href="./manProject"><button className='manMain-button'>프로젝트 관리</button></a>
+      <a className='manMain-a' href="./manApply"><button className='manMain-button'>모집 관리</button></a>
     </div>
   )
 }
