29d62f3f3bcbe6ad7048eee49ebafa82126bd0a1
yewon_lee <dapang14@naver.com>
2024-01-18 22:25:06 +0900

    back 연결중2

--- src/pages/SignUpPage/SignUp1.jsx
+++ src/pages/SignUpPage/SignUp1.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "./SignUp1.css";
-import { Link, useNavigate } from 'react-router-dom'
-
-
 
 export default function SignUp1() {
-  
+  const [authCode, setAuthCode] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:8090/SignUp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ authCode }),
+      });
+
+      if (response.ok) {
+        // 인증 성공
+        console.log('인증 성공');
+      } else {
+        // 인증 실패 처리
+        console.error('인증 실패');
+      }
+    } catch (error) {
+      // 네트워크 또는 기타 오류 처리
+      console.error('에러:', error);
+    }
+  };
+
   return (
     <div className='SignUp1Page'>
       {/* <div className="Element">
@@ -18,15 +40,26 @@ export default function SignUp1() {
       <div className="certification">부원 인증</div>
       <div className="inputInfo">부원 정보 입력</div>
       </div> */}
-    <div className='SignUp1Title'>DASOM</div>
-    <div className='SignUp1_form'>
-    <div className='SignUp1_check'>이미 회원이신가요?</div>
-    <Link to='/Login' className='SignUp1_login'>로그인 하기</Link>
+      <div className='SignUp1Title'>DASOM</div>
+      <div className='SignUp1_form'>
+        <div className='SignUp1_check'>이미 회원이신가요?</div>
+        <Link to='/Login' className='SignUp1_login'>
+          로그인 하기
+        </Link>
+      </div>
+      <input
+        className='SignUp1_input'
+        type='text'
+        placeholder='인증번호를 입력하세요.'
+        value={authCode}
+        onChange={(e) => setAuthCode(e.target.value)}>
+        </input>
+      <button
+        className='SignUp01_certification'
+        onClick={handleSubmit}
+      >
+        다솜 부원 인증하기
+      </button>
     </div>
-    <input class="SignUp1_input" type='text' placeholder='인증번호를 입력하세요.'></input>
-    <button className='SignUp01_certification'>다솜 부원 인증하기</button>
-    
-    </div>
-   
   );
 }
