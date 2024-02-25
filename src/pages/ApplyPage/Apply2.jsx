842e1cdeece3b7f24bee4eeb382c7f81a3885ce3
yewon_lee <dapang14@naver.com>
2024-02-26 03:32:12 +0900

    apply02 기수 수정

--- src/pages/ApplyPage/Apply2.jsx
+++ src/pages/ApplyPage/Apply2.jsx
@@ -18,12 +18,12 @@ function Apply2() {
       <div id="Ap2-first-forms" class="form">
 
         <div>
-        <p id="Ap2-form-title">DASOM <span class="Ap2-mini-text">32</span>nd</p>
+        <p id="Ap2-form-title">DASOM <span class="Ap2-mini-text">33</span>nd</p>
         <Ap2NullBox></Ap2NullBox>
         <p id="Ap2-sub-title">
-          다솜 32기에 지원해 주셔서 감사합니다.,<br></br>
+          다솜 33기에 지원해 주셔서 감사합니다.,<br></br>
           작성해 주신 지원서는 검토 후<br></br>
-          서류 결과 발표일에 <b>문자로 연락</b>드리겠습니다.<br></br><br></br>
+          서류 결과 발표일에 <b>개별로 연락</b>드리겠습니다.<br></br><br></br>
           감사합니다.</p>
         </div>
       </div>
